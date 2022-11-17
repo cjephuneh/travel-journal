c261d7b20e40920f021c4589663a91a89607de7f
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-17 12:32:42 +0300

    vv

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   return (
-    <h1>hello world lets build the next facebook</h1>
+    <h1>hello</h1>
   )
 }
 
