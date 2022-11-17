2f68cf8ec19ce0d930e83994e297e81d1c9f2aeb
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-17 12:27:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <h1>hello world lets build the next facebook</h1>
+  )
+}
+
+export default App
