e383ce6e79727c67885b6ded4d5088af25b8ea34
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-18 20:27:25 +0300

    cvf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from './components/Navbar'
-
+import Hero from './components/Hero'
 import './App.css'
 
 function App() {
   return (
     <>
      <Navbar />
+     <Hero />
     </>
   )
 }
